431f69145da45ac322578fab858a184cfd1c193e
Leonardo Herrera Ornelas <leonardoherreraornelas960302@gmail.com>
2018-05-27 12:41:51 -0500

    create new file to describe tree_node struct

--- grammar_attributes.h
+++ grammar_attributes.h
@@ -3,4 +3,6 @@
 
 typedef enum {INTEGER_SYMBOL, REAL_SYMBOL, VOID_SYMBOL, ERROR_SYMBOL} SemanticType;
 
+SemanticType getNumericType(SemanticType t1, SemanticType t2);
+
 #endif // SEMANTIC_TYPE_H_INCLUDED
--- semantic_analyzer.c
+++ semantic_analyzer.c
@@ -10,7 +10,7 @@ void analyzeSemantics(SyntaxTreeNode** treeRoot, SymbolTableNode** symbolTable)
         SyntaxTreeNode* aux;
         for (aux = *treeRoot; aux != NULL; aux = aux->next)
         {
-            calculateTypeForStatement(aux);
+            calculateAttributesForStatement(aux);
             if (aux->semanticType == ERROR_SYMBOL)
                 errorExists = 1;
         }
@@ -21,15 +21,15 @@ void analyzeSemantics(SyntaxTreeNode** treeRoot, SymbolTableNode** symbolTable)
     }
 }
 
-void calculateTypeForStatement(SyntaxTreeNode* statement)
+void calculateAttributesForStatement(SyntaxTreeNode* statement)
 {
     int statementType = statement->ruleType;
     switch(statementType)
     {
     case IF_STATEMENT_NODE:
-        calculateTypeForExpression(statement->expression);
-        calculateTypeForBlock(statement->ifPart);
-        calculateTypeForBlock(statement->elsePart);
+        calculateAttributesForExpression(statement->expression);
+        calculateAttributesForBlock(statement->ifPart);
+        calculateAttributesForBlock(statement->elsePart);
 
         statement->semanticType = ((statement->expression->semanticType != ERROR_SYMBOL)
                                    && (statement->ifPart == NULL || statement->ifPart->semanticType == VOID_SYMBOL)
@@ -37,8 +37,8 @@ void calculateTypeForStatement(SyntaxTreeNode* statement)
                                   ? VOID_SYMBOL : ERROR_SYMBOL;
         break;
     case WHILE_STATEMENT_NODE:
-        calculateTypeForExpression(statement->expression);
-        calculateTypeForBlock(statement->blockStatements);
+        calculateAttributesForExpression(statement->expression);
+        calculateAttributesForBlock(statement->blockStatements);
 
         statement->semanticType = ((statement->expression->semanticType != ERROR_SYMBOL)
                                    && (statement->blockStatements == NULL || statement->blockStatements->semanticType == VOID_SYMBOL))
@@ -48,11 +48,11 @@ void calculateTypeForStatement(SyntaxTreeNode* statement)
         //TODO: print const string node
         break;
     case PRINT_EXPRESSION_STATEMENT_NODE:
-        calculateTypeForExpression(statement->expression);
+        calculateAttributesForExpression(statement->expression);
         statement->semanticType = (statement->expression->semanticType != ERROR_SYMBOL) ? VOID_SYMBOL : ERROR_SYMBOL;
         break;
     case ASSIGNMENT_STATEMENT_NODE:
-        calculateTypeForExpression(statement->expression);
+        calculateAttributesForExpression(statement->expression);
         if (statement->expression->semanticType != ERROR_SYMBOL)
         {
             if (existsNode(&symbolTableHeader, statement->identifier->symbol))
@@ -66,7 +66,7 @@ void calculateTypeForStatement(SyntaxTreeNode* statement)
     }
 }
 
-void calculateTypeForExpression(SyntaxTreeNode* expression)
+void calculateAttributesForExpression(SyntaxTreeNode* expression)
 {
     int expressionType = expression->ruleType;
     switch(expressionType)
@@ -76,20 +76,13 @@ void calculateTypeForExpression(SyntaxTreeNode* expression)
     case EQUALS_COMPARISON_OPERATION_NODE:
     case RELATIONAL_OPERATION_NODE:
     case AND_OPERATION_NODE:
-        calculateTypeForExpression(expression->operand1);
-        calculateTypeForExpression(expression->operand2);
-
-        if (expression->operand1->semanticType == INTEGER_SYMBOL && expression->operand2->semanticType == INTEGER_SYMBOL)
-            expression->semanticType = INTEGER_SYMBOL;
-        else if (expression->operand2->semanticType == REAL_SYMBOL && expression->operand2->semanticType == REAL_SYMBOL)
-            expression->semanticType = REAL_SYMBOL;
-        else
-            expression->semanticType = ERROR_SYMBOL;
-
+        calculateAttributesForExpression(expression->operand1);
+        calculateAttributesForExpression(expression->operand2);
+        expression->semanticType = getNumericType(expression->operand1->semanticType, expression->operand2->semanticType);
         break;
     case UNARY_OPERATION_NODE:
     case NEGATION_OPERATION_NODE:
-        calculateTypeForExpression(expression->expression);
+        calculateAttributesForExpression(expression->expression);
         expression->semanticType = (expression->expression->semanticType != ERROR_SYMBOL) ? expression->expression->semanticType : ERROR_SYMBOL;
         break;
     case IDENTIFIER_NODE:
@@ -103,7 +96,7 @@ void calculateTypeForExpression(SyntaxTreeNode* expression)
     }
 }
 
-void calculateTypeForBlock(SyntaxTreeNode* block)
+void calculateAttributesForBlock(SyntaxTreeNode* block)
 {
     if (block != NULL)
     {
@@ -111,7 +104,7 @@ void calculateTypeForBlock(SyntaxTreeNode* block)
         SyntaxTreeNode* aux;
         for (aux = block; aux != NULL; aux = aux->next)
         {
-            calculateTypeForStatement(aux);
+            calculateAttributesForStatement(aux);
             if (aux->semanticType == ERROR_SYMBOL)
                 errorExists = 1;
         }
