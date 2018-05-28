14912080ee0d1006190d1b13db56549f97d9f246
Leonardo Herrera Ornelas <leonardoherreraornelas960302@gmail.com>
2018-05-28 15:10:00 -0500

    WIP: Quit not neccesary boolean value, modify some functions to correctly data type casting and continue refactoring semantic analyzer

--- salida.asm
+++ salida.asm
@@ -8,40 +8,41 @@ include \masm32\macros\macros.asm
 includelib \masm32\lib\masm32.lib
 includelib \masm32\lib\kernel32.lib
 
-.data
-_So3a dword 0
-_R dword 0
-_e dword 0
-_l508 dword 0
-_Va dword 0
-_xg dword 0
-
 .code
 begin:
-push 72
+push 7.2
 push 3.5
 pop ebx
 pop eax
 add eax, ebx
 push eax
-push 2
+push 2.8
 pop ebx
 pop eax
 add eax, ebx
 push eax
 pop _So3a
-push 02
-pop _R
-push 52
-pop _e
-push 1
-pop _l508
-push 5
-pop _Va
 push 3
-pop _xg
-push _e
+push 4
+pop ebx
 pop eax
-print str$(eax), 13, 10
+cmp eax, ebx
+jg T0
+push 0
+jmp E0
+T0:
+push 1
+E0:
+push _So3a
+pop ebx
+pop eax
+cmp eax, ebx
+jl T1
+push 0
+jmp E1
+T1:
+push 1
+E1:
+pop _b
 exit
 end begin
