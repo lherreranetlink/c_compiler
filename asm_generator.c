c612bdc74a108104e67254e6d8a4288f78420043
Leonardo Herrera Ornelas <lherrera@grupoplexon.com>
2019-04-04 16:52:31 -0600

    yo tambien agregue un printf

--- asm_generator.c
+++ asm_generator.c
@@ -14,6 +14,7 @@ void generate_asm(SyntaxTreeNode** treeRoot, SymbolTableNode** symbolTable)
         generateDataSection();
         fprintf(outFd, ".code\n");
         fprintf(outFd, "begin:\n");
+        fprintf(outFd, "begiiin:\n");
         SyntaxTreeNode* aux;
         for (aux = *treeRoot; aux != NULL; aux = aux->next)
             generateStatementCode(aux);
